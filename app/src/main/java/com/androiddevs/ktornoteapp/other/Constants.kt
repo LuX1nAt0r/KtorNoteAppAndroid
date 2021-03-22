7ef6136051928fde58ba235b0a8ebc7958e2f156
LuX1nAt0r <l.landsteiner@t-online.de>
2021-03-22 21:01:29 +0100

    Sat up daggerhilt

--- app/src/main/java/com/androiddevs/ktornoteapp/other/Constants.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/other/Constants.kt
@@ -4,5 +4,9 @@ object Constants {
 
     const val DATABASE_NAME = "notes_db"
 
+    const val BASE_URL = "http://192.168.0.102:8001"
+
+    const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
+
     val IGNORE_AUTH_URLS = listOf("/login", "/register")
 }
