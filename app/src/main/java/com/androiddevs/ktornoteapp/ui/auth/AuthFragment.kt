444ee3d65bfb447762406db90f4acb7ba72083bd
LuX1nAt0r <l.landsteiner@t-online.de>
2021-04-18 14:06:33 +0200

    Logging out and staying logged in is working

--- app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
@@ -12,6 +12,8 @@ import com.androiddevs.ktornoteapp.R
 import com.androiddevs.ktornoteapp.data.remote.BasicAuthInterceptor
 import com.androiddevs.ktornoteapp.other.Constants.KEY_LOGGED_IN_EMAIL
 import com.androiddevs.ktornoteapp.other.Constants.KEY_PASSWORD
+import com.androiddevs.ktornoteapp.other.Constants.NO_EMAIL
+import com.androiddevs.ktornoteapp.other.Constants.NO_PASSWORD
 import com.androiddevs.ktornoteapp.other.Status
 import com.androiddevs.ktornoteapp.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,6 +38,11 @@ class AuthFragment: BaseFragment(R.layout.fragment_auth) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if(isLoggedIn()){
+            authenticateApi(curEmail ?: "", curPassword ?: "")
+            redirectLogin()
+        }
+
         requireActivity().requestedOrientation = SCREEN_ORIENTATION_PORTRAIT
         subscribeToObservers()
 
@@ -54,8 +61,14 @@ class AuthFragment: BaseFragment(R.layout.fragment_auth) {
             viewModel.login(email, password)
         }
 
+    }
 
 
+    private fun isLoggedIn(): Boolean {
+        curEmail = sharedPref.getString(KEY_LOGGED_IN_EMAIL, NO_EMAIL) ?: NO_EMAIL
+        curPassword = sharedPref.getString(KEY_PASSWORD, NO_PASSWORD) ?: NO_PASSWORD
+
+        return curEmail != NO_EMAIL && curPassword != NO_PASSWORD
     }
 
 
